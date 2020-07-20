817cab5268c44a62a3e4e1d90e419b3949813a78
Manas Kanithahalli Sadananda <manas.ks15@gmail.com>
2020-07-19 19:09:33 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,14 +1,18 @@
 #/usr/bin/python
-DEBUG = 0
-SUM = 0
+DEBUG = None
+sum = 0
 number = 0
-for number in range(1,1000):
-    if (number%3==0) and (number%5==0):
-        if DEBUG:
-            print "Number: ", number
-            print "%3: ",number%3
-            print "%5: ",number%5
-        SUM = SUM + number
-    else:
-        continue
-print "TOTAL SUM: ", SUM
+def problem1():
+    for number in range(1000):
+        if (number%3==0) and (number%5==0):
+            if DEBUG:
+                print("Number: ", number)
+                print("%3: ",number%3)
+                print("%5: ",number%5)
+            sum = sum + number
+        else:
+            continue
+    print "TOTAL sum: ", sum
+
+if __name__ == "__main__":
+    problem1()
